bdddc2a887135848e1f306dc2ed4a4e41f2a5ec0
leadrian <thewishwithin@gmail.com>
2019-04-20 02:17:12 +0200

    Added binding for FCNPCCallbacks

--- src/main/kotlin/ch/leadrian/samp/kamp/fcnpcwrapper/FCNPCModule.kt
+++ src/main/kotlin/ch/leadrian/samp/kamp/fcnpcwrapper/FCNPCModule.kt
@@ -1,6 +1,7 @@
 package ch.leadrian.samp.kamp.fcnpcwrapper
 
 import ch.leadrian.samp.kamp.core.api.inject.KampModule
+import ch.leadrian.samp.kamp.fcnpcwrapper.callback.FCNPCCallbackProcessor
 import ch.leadrian.samp.kamp.fcnpcwrapper.entity.registry.MovePathRegistry
 import ch.leadrian.samp.kamp.fcnpcwrapper.entity.registry.MovePathRegistryProvider
 import ch.leadrian.samp.kamp.fcnpcwrapper.entity.registry.PlaybackRecordRegistry
@@ -10,6 +11,7 @@ class FCNPCModule : KampModule() {
 
     override fun configure() {
         bind(FCNPCCallbackManager::class.java).asEagerSingleton()
+        bind(FCNPCCallbacks::class.java).to(FCNPCCallbackProcessor::class.java)
         bind(MovePathRegistry::class.java).toProvider(MovePathRegistryProvider::class.java)
         bind(PlaybackRecordRegistry::class.java).toProvider(PlaybackRecordRegistryProvider::class.java)
     }
